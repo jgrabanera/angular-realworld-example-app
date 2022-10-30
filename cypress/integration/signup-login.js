80b4144074625cce7859eabba90c9fe3406982a1
jgrabanera <28018937+jgrabanera@users.noreply.github.com>
2022-10-30 21:36:55 +0800

    added assertion on XHR Request

--- cypress/integration/signup-login.js
+++ cypress/integration/signup-login.js
@@ -2,19 +2,31 @@
 
 describe("Signup & Login", () => {
   let randomString = Math.random().toString(36).substring(2);
+  let username = "auto" + randomString;
+  let emailTest = "auto" + randomString + "@gmail.com";
 
   it("Test valid signup", () => {
+    cy.intercept("POST", "**/*.realworld.io/api/users").as("newUser");
+
     cy.visit("http://localhost:4200/");
 
     //cy.get(".container > .nav > :nth-child(3) > .nav-link");
     cy.get(".nav").contains("Sign up").click();
 
     //cy.get(":nth-child(1) > .form-control");
-    cy.get("input[formcontrolname='username'").type("auto" + randomString); //jomz
-    cy.get("input[formcontrolname='email'").type(
-      "autoEmail" + randomString + "@testing.com"
-    ); //jomz@testing.com
+    cy.get("input[formcontrolname='username'").type(username); //jomz
+    cy.get("input[formcontrolname='email'").type(emailTest); //jomz@testing.com
     cy.get("input[formcontrolname='password'").type("autoPass" + randomString); //cy.Testing01
     cy.get("button.btn").contains("Sign up").click();
+
+    cy.wait("@newUser").should(({ request, response }) => {
+      cy.log("Request: " + JSON.stringify(request));
+      cy.log("Response: " + JSON.stringify(response));
+
+      expect(response.statusCode).to.eq(200);
+
+      expect(request.body.user.username).to.include(username); //assert username
+      expect(request.body.user.email).to.include(emailTest); // assert email
+    });
   });
 });
