ddae55b761209ea168ae9ffc588edde18deeaec1
jgrabanera <28018937+jgrabanera@users.noreply.github.com>
2022-10-30 22:35:13 +0800

    added individual test for automated login

--- cypress/integration/signup-login.js
+++ cypress/integration/signup-login.js
@@ -4,6 +4,7 @@ describe("Signup & Login", () => {
   let randomString = Math.random().toString(36).substring(2);
   let username = "auto" + randomString;
   let emailTest = "auto" + randomString + "@gmail.com";
+  let passTest = "autoPass" + randomString;
 
   it("Test valid signup", () => {
     cy.intercept("POST", "**/*.realworld.io/api/users").as("newUser");
@@ -14,9 +15,9 @@ describe("Signup & Login", () => {
     cy.get(".nav").contains("Sign up").click();
 
     //cy.get(":nth-child(1) > .form-control");
-    cy.get("input[formcontrolname='username'").type(username); //jomz
-    cy.get("input[formcontrolname='email'").type(emailTest); //jomz@testing.com
-    cy.get("input[formcontrolname='password'").type("autoPass" + randomString); //cy.Testing01
+    cy.get("input[formcontrolname='username']").type(username); //jomz
+    cy.get("input[formcontrolname='email']").type(emailTest); //jomz@testing.com
+    cy.get("input[formcontrolname='password']").type(passTest); //cy.Testing01
     cy.get("button.btn").contains("Sign up").click();
 
     cy.wait("@newUser").should(({ request, response }) => {
@@ -24,9 +25,35 @@ describe("Signup & Login", () => {
       cy.log("Response: " + JSON.stringify(response));
 
       expect(response.statusCode).to.eq(200);
-
       expect(request.body.user.username).to.include(username); //assert username
       expect(request.body.user.email).to.include(emailTest); // assert email
+
+      //cy.get("a[href='/settings']").click();
+      //cy.get(".btn").contains("Or click here to logout.").click();
     });
+  });
+
+  it("Test valid login", () => {
+    //cy.intercept("POST", "**/*.realworld.io/api/users").as("newUser");
+
+    cy.visit("http://localhost:4200/");
+
+    cy.get(".nav").contains("Sign in").click();
+    cy.get("input[formcontrolname='email']").type(emailTest);
+    cy.get("input[formcontrolname='password']").type(passTest);
+    cy.get("button.btn").contains("Sign in").click();
+
+    //assetion
+    cy.get(":nth-child(4) > .nav-link").should("contain", username);
+
+    // cy.wait("@newUser").should(({ request, response }) => {
+    //   cy.log("Request: " + JSON.stringify(request));
+    //   cy.log("Response: " + JSON.stringify(response));
+
+    //   expect(response.statusCode).to.eq(200);
+
+    //   expect(request.body.user.username).to.include(username); //assert username
+    //   expect(request.body.user.email).to.include(emailTest); // assert email
+    // });
   });
 });
